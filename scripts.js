fe031dca9a783ed0b6e41fa58f0b0cb995656d48
peram015 <peram015@newschool.edu>
2015-10-23 13:14:36 +0000

    Increasing Roses

---
+++ scripts.js
@@ -0,0 +1,21 @@
+console.log("
+console.log("");
+
+var symbols = ["@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~", "@~ @~"]
+
+var pattern = "@~";
+
+for var increment= 0; increment < symbols.length; increment++) {
+    console.log(pattern);
+    pattern = pattern + symbols[increment];
+    var ampersand = "";
+    console.log(increment);
+    
+    if (increment > 14)
+        for (var another = 0; another < 20; another++) {
+            console.log(ampersand);
+            ampersand = ampersand + ["-", "-"]
+            
+        }
+}
+
