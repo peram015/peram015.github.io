7ba16f4e3d3256163c1668009e0c443c630672c0
peram015 <peram015@newschool.edu>
2015-12-18 18:29:48 +0000

    final

---
+++ final.js
@@ -0,0 +1,27 @@
+
+
+var clicked = false;
+
+$("#container").click(function() {
+    if (clicked == false) {
+    $(".skinny").show();
+    $(".fat").hide();
+    shown = true;
+    } else {
+    $(".skinny").hide();
+    $(".fat").show();
+    shown = false;
+    }
+});
+    
+    var clicked = false;
+    
+$("#container").click(function)() {
+    if (clicked == false) {
+        $(".stomach").css("width" "1000")
+        shown=true;
+    } else {
+    $(".stomach").css("width "2000")
+    }
+    shown= false;
+}
